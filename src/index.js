b40035f6d62b07028ee61e8f65aec6979aea109b
Andrei Tarasevich <eansable@yandex.by>
2020-10-02 15:46:31 +0300

    init: create function min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
+exports.min = function min(array) {
+    let i = 0;
+    let minElement = array[0];
 
-exports.min = function min (array) {
-  return 0;
-}
+    while (i <= array.length - 1) {
+        if (array[i] < minElement) {
+            minElement = array[i];
+        }
+        i++;
+    }
+    return minElement;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    let i = 0;
+    let maxElement = array[0];
+    while (i <= array.length - 1) {
+        if (array[i] > maxElement) {
+            maxElement = array[i];
+        }
+        i++;
+    }
+    return maxElement;
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    let i = 0;
+    let sum = 0;
+    if (array === 0) return 0;
+    while (i < array.length) {
+        sum += array[i];
+        i++;
+    }
+    return sum / array.length;
+};
